fc56be14e8975b3d6514c53c89433f5f65fdb84f
shanusingh01 <137302062+shanusingh01@users.noreply.github.com>
2024-08-03 12:01:48 +0530

    Add files via upload

---
+++ Calculator/calc.js
@@ -0,0 +1,26 @@
+const screenDisplay = document.querySelector('.screen')
+const buttons = document.querySelectorAll('button')
+
+let calculation = []
+let accumlativeCalculation
+
+
+function calculate(button) {
+    const value = button.textContent
+
+    if (value == "CLEAR") {
+        calculation = []
+        screenDisplay.textContent = '0'
+    }
+    else if(value === "="){
+        screenDisplay.textContent = eval(accumlativeCalculation)
+    }
+    else {
+        calculation.push(value)
+        accumlativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumlativeCalculation
+    }
+
+}
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
